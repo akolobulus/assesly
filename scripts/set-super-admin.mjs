36b0b914e6ee0a52b3f28093be9ccfa5918208b8
Akolo Bulus <officialarikpa@gmail.com>
2026-09-10 13:53:20 +0100

    Update set-super-admin.mjs

--- scripts/set-super-admin.mjs
+++ scripts/set-super-admin.mjs
@@ -10,8 +10,7 @@
 // 3. Get the User UID of the user you want to make a super admin:
 //    - Go to Firebase console > Authentication > Users.
 //    - Copy the UID for the desired user.
-// 4. Update the `userUid` and `serviceAccountPath` variables below with your values.
-// 5. Run the
+// 4. Update the `userUid` and `serviceAccoun
 
 import admin from 'firebase-admin';
 // The 'fs' module is used to read the service account file.
